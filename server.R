e81f8d683be0df3344d55d34e57c1a1ed7843e25
Corentin Bect <corentin.bect@gmail.com>
2017-06-07 23:20:08 +0200

    Choix d'une loi de distribution qui sera comparée à l'échantillon

--- server.R
+++ server.R
@@ -36,18 +36,114 @@ shinyServer(function(input, output) {
     plot(ecdf(data2), add=TRUE, col="red")
   })
   output$text <- renderUI({
+    c <- input$choiceRb
     f1 <- input$csvfile1
     if (is.null(f1))
       return(NULL)
-    f2 <- input$csvfile2
-    if (is.null(f2))
-      return(NULL)
     data1 <- read.csv(f1$datapath, header=FALSE, sep=",", quote="", dec=".", fill=FALSE, comment.char="")
-    data2 <- read.csv(f2$datapath, header=FALSE, sep=",", quote="", dec=".", fill=FALSE, comment.char="")
-    ksdata <- ks.test(as.numeric(data1), as.numeric(data2))
-    HTML(paste("Results of the test",
-          sprintf("D: %s", format(ksdata[1], digits=5)),
-          sprintf("p-value: %s",  format(ksdata[2], digits=5)),
-          sep='<br/>'))
+    if (c == "0"){
+      f2 <- input$csvfile2
+      if (is.null(f2))
+        return(NULL)
+      data2 <- read.csv(f2$datapath, header=FALSE, sep=",", quote="", dec=".", fill=FALSE, comment.char="")
+      ksdata <- ks.test(as.numeric(data1), as.numeric(data2))
+      HTML(paste("Results of the test",
+                 sprintf("D: %s", format(ksdata[1], digits=5)),
+                 sprintf("p-value: %s",  format(ksdata[2], digits=5)),
+                 sep='<br/>'))
+    }else{
+      if(input$df != "None"){
+        distrib <- switch(
+          input$df,
+          "Beta" = "pbeta",
+          "Binomial" = "pbinom",
+          "Cauchy" = "pcauchy",
+          "Chi-Square" = "pchisq",
+          "Exponential" = "pexp",
+          "F" = "pf",
+          "Gamma" = "pgamma",
+          "Geometric" = "pgeom",
+          "Hypergeometric" = "phyper",
+          "Logistic" = "plogis",
+          "Log normal" = "plnorm",
+          "Negative Binomial" = "pnbinom",
+          "Normal" = "pnorm",
+          "Poisson" = "ppois",
+          "Student" = "pt",
+          "Stuendentized Range" = "ptukey",
+          "Uniform" = "punif",
+          "Weibull" = "pweibull",
+          "Wilcoxon Rank Sum Statistic" = "pwilcox",
+          "Wilcoxon Signed Rank Statistic" = "psignrank"
+        )
+        nbParameters <- as.numeric(datasetInput2())
+        if(nbParameters == 0){
+          ksdata <- ks.test(as.numeric(data1), distrib)
+        }
+        if(nbParameters == 1){
+             ksdata <- ks.test(as.numeric(data1), distrib, as.numeric(input$parameter1))
+        }
+        if(nbParameters == 2){
+             ksdata <- ks.test(as.numeric(data1), toString(distrib), 
+                               as.numeric(input$parameter1),
+                               as.numeric(input$parameter2))   
+        } 
+        if(nbParameters == 3){
+          p1 <- input$parameter1
+          p2 <- input$parameter2
+             ksdata <- ks.test(as.numeric(data1), toString(distrib), 
+                               as.numeric(p1),
+                               as.numeric(p2))  
+        }  
+        
+        HTML(paste("Results of the test",
+                   sprintf("D: %s", format(ksdata[1], digits=5)),
+                   sprintf("p-value: %s",  format(ksdata[2], digits=5)),
+                   sep='<br/>'))
+      }
+    }
   })
+  
+  datasetInput <- reactive({
+    switch(input$choiceRb,
+           "0" = TRUE,
+           "1" = FALSE)
+  })
+  
+  output$choice <- reactive({
+    datasetInput()
+  })
+  
+  outputOptions(output, "choice", suspendWhenHidden = FALSE)  
+  
+  datasetInput2 <- reactive({
+    switch(input$df,
+           "Beta" = 2,
+           "Binomial" = 2,
+           "Cauchy" = 2,
+           "Chi-Square" = 1,
+           "Exponential" = 1,
+           "F" = 2,
+           "Gamma" = 2,
+           "Geometric" = 1,
+           "Hypergeometric" = 3,
+           "Logistic" = 2,
+           "Log normal" = 2,
+           "Negative Binomial" = 3,
+           "Normal" = 2,
+           "Poisson" = 1,
+           "Student" = 1,
+           "Stuendentized Range" = 2,
+           "Uniform" = 2,
+           "Weibull" = 2,
+           "Wilcoxon Rank Sum Statistic" = 2,
+           "Wilcoxon Signed Rank Statistic" = 1)
+  })
+  
+  output$parameters <- reactive({
+    datasetInput2()
+  })
+  
+  outputOptions(output, "parameters", suspendWhenHidden = 0)  
+  
 })
--- ui.R
+++ ui.R
@@ -7,7 +7,7 @@ shinyUI(fluidPage(theme = shinytheme("darkly"),
 
   fluidRow(
     column(4,
-           fileInput("csvfile1", "Choose CSV File",
+           fileInput("csvfile1", "First sample : choose CSV File",
                      accept = c(
                        "text/csv",
                        ".csv")
@@ -19,16 +19,68 @@ shinyUI(fluidPage(theme = shinytheme("darkly"),
   ),
   fluidRow(
     column(4,
-           fileInput("csvfile2", "Choose CSV File",
-                     accept = c(
-                       "text/csv",
-                       ".csv")
-           )      
+           radioButtons("choiceRb", "Choose an element to compare to the first sample:",
+                        choiceNames = list(
+                          "Import another sample",
+                          "Choose a distribution function"
+                        ),
+                        choiceValues = list(
+                          "0", "1"
+                        ))     
     ),
     column(8,
-           tableOutput('cvsdata2')
+           textOutput('choiceRb')
     )
   ),
+  fluidRow(
+    column(4, conditionalPanel( condition = "output.choice == true",
+                                fileInput("csvfile2", "Choose CSV File",
+                                          accept = c(
+                                            "text/csv",
+                                            ".csv")
+                                ) )     
+    ),
+    column(8,
+             textOutput('cvsdata2')
+           
+    )
+  ),
+  fluidRow(
+    column(3, conditionalPanel( condition = "output.choice == false",
+                                          selectInput(inputId = "df",
+                                                      label = "Distribution function:",
+                                                      choices = c('None', 'Beta', 'Binomial', 'Cauchy', 'Chi-Square',
+                                                                  'Exponential', 'F', 'Gamma', 'Geometric',
+                                                                  'Hypergeometric', 'Logistic', 'Log normal',
+                                                                  'Negative Binomial', 'Normal', 'Poisson',
+                                                                  'Student', 'Stuendentized Range', 'Uniform',
+                                                                  'Weibull', 'Wilcoxon Rank Sum Statistic',
+                                                                  'Wilcoxon Signed Rank Statistic'),
+                                                      selected = 'None') )   
+           
+           
+           
+           
+    ),
+    column(3,
+           conditionalPanel( condition = "output.choice == false && output.parameters >= 1",
+                             textInput("parameter1", "First parameter", width = 150) )  
+           
+    ),
+    column(3,
+           conditionalPanel( condition = "output.choice == false && output.parameters >= 2",
+                             textInput("parameter2", "Second parameter", width = 150) )  
+           
+    )),
+  column(3,
+         conditionalPanel( condition = "output.choice == false && output.parameters >= 3",
+                           textInput("parameter3", "Third parameter", width = 150) )  
+         
+  )
+    
+  ),
+  
+  
   fluidRow(
     column(12,
            plotOutput('plot')
